c02c5b2f9dd7397868018baff570a8cab88a1a61
Ezekiel Wotring <40004347+KAJdev@users.noreply.github.com>
2025-11-06 14:31:15 -0900

    Refactor handler function by removing sleep and log
    Removed simulated delay and unnecessary log print.

--- src/handler.py
+++ src/handler.py
@@ -4,12 +4,6 @@ import subprocess
 
 def handler(job):
     """get CUDA version"""
-
-    # sleep for 10 seconds to simulate a long running job
-    time.sleep(10)
-
-    print("this is a log that should be captured by sls-local-server")
-
     output = subprocess.check_output(["nvcc", "--version"]).decode("utf-8")
     print(output)
     return output.split("\n")[3]
